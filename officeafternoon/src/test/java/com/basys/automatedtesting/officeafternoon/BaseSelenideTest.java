f2cda7feff64e5d0bd48fe0dd79240b06edf6719
kreMarc <102742175+kreMarc@users.noreply.github.com>
2022-03-31 15:53:58 +0200

    Add files via upload

---
+++ officeafternoon/src/test/java/com/basys/automatedtesting/officeafternoon/BaseSelenideTest.java
@@ -0,0 +1,31 @@
+package com.basys.automatedtesting.officeafternoon;
+
+import static com.codeborne.selenide.Selenide.open;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+
+public class BaseSelenideTest {
+
+	@BeforeEach
+	public void start() {
+
+		System.setProperty("webdriver.gecko.driver", "C:\\Entwicklung\\eclipse\\webdrivers\\geckodriver.exe");
+		Configuration.browser = "FIREFOX";
+
+//		Configuration.timeout = 5000;
+//		Configuration.pollingInterval = 500;
+		open("https://www.google.de");
+		WebDriverRunner.getWebDriver().manage().window().maximize();
+
+	}
+
+	@AfterEach
+	public void quit() {
+		WebDriverRunner.closeWebDriver();
+	}
+
+}
